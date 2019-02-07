182cc172231a2eb102c3c9fd7a93b1182508ae1b
Lukas Šivickas <l.sivickas@gmail.com>
2019-02-07 21:10:03 +0200

    Add performExchange method to IMainPresenter

--- app/src/main/java/com/lusivic/currencyexchange/ui/main/presenter/IMainPresenter.kt
+++ app/src/main/java/com/lusivic/currencyexchange/ui/main/presenter/IMainPresenter.kt
@@ -1,9 +1,11 @@
 package com.lusivic.currencyexchange.ui.main.presenter
 
 import com.lusivic.currencyexchange.ui.base.presenter.IPresenter
-import com.lusivic.currencyexchange.ui.main.view.IMainActivity
 import com.lusivic.currencyexchange.ui.main.interactor.IMainInteractor
+import com.lusivic.currencyexchange.ui.main.view.IMainActivity
 
 interface IMainPresenter<V: IMainActivity, I: IMainInteractor>: IPresenter<V, I> {
+
+    fun performExchange(amount: Double, currencyFrom: String, currencyTo: String)
 
 }
